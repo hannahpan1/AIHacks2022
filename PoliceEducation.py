e4cbd57b6a44836855ba49898335f78a543a68f2
MehBirb <john.lu10212004@gmail.com>
2022-10-01 12:57:46 -0400

    In the process of building blueprints

--- PoliceEducation.py
+++ PoliceEducation.py
@@ -1,2 +1,19 @@
 def snake_case(inputFile):
     
+def node_1_output(input):
+
+def node_2_output(input):
+
+
+
+
+
+
+#First, we get the input
+
+#Then we put it in a while loop
+    #Do simple forward propogatoin
+    
+    #Now we have a list
+    
+    #Use the list to see if it gets the right answers
